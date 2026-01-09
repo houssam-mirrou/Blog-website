e86bc2f2c8567ffed8777156cb8d59c4d47e8ac5
houssam-mirrou <hossam.mirrou@gmail.com>
2026-01-09 10:18:21 +0100

    bugs/explore-page this commit constains an update of the home page to show user and stuff based on the user who will see the website

--- src/App/Controllers/CreateArticleController.php
+++ src/App/Controllers/CreateArticleController.php
@@ -9,32 +9,40 @@ use App\Repositories\UserRepository;
 use App\Services\ArticleServices;
 use App\Services\CategoryServices;
 
-class CreateArticleController extends Controller {
-    public function index() {
-        if(!isset($_SESSION['current_user'])){
+class CreateArticleController extends Controller
+{
+    public function index()
+    {
+        if (!isset($_SESSION['current_user'])) {
             header('Location: /');
+            exit();
+        }
+        if ($_SESSION['current_user']->get_role() == 'admin') {
+            header('Location: /');
+            exit();
         }
         $cat_services = new CategoryServices();
         $db_categories = $cat_services->get_all_category();
         $categories = [];
-        foreach($db_categories as $cat){
-            $category = new Category($cat['name'],$cat['slug'],$cat['id']);
-            array_push($categories,$category);
+        foreach ($db_categories as $cat) {
+            $category = new Category($cat['name'], $cat['slug'], $cat['id']);
+            array_push($categories, $category);
         }
-        $this->view('create-article',[
-            'title'=>'Create Article',
-            'categories'=>$categories
+        $this->view('create-article', [
+            'title' => 'Create Article',
+            'categories' => $categories
         ]);
     }
-    public function post_article(){
+    public function post_article()
+    {
         $user_repositry = new UserRepository();
         $article_services = new ArticleServices();
         $title = $_POST['title'];
         $body = $_POST['content'];
         $category_ids = $_POST['category_ids'] ?? [];
         $author_id = $user_repositry->get_user_id($_SESSION['current_user']->get_email());
-        $result = $article_services->add_article($title,$body,$author_id,$category_ids);
-        if($result === true){
+        $result = $article_services->add_article($title, $body, $author_id, $category_ids);
+        if ($result === true) {
             header('Location: /dashboard-author');
             exit();
         }
